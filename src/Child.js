2a9acef3ca382a9dd7c3b282110e48669af01cf2
kashifnawaz <kashi030@hotmail.com>
2021-04-15 15:22:12 +0500

    Created

---
+++ src/Child.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function App(props) {
+  return (
+    <div>
+     <Child></Child>
+    </div>
+    
+    
+  );
+}
+
+export default Child;
---
+++ src/Parent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Parent from 'Child.js';
+
+function App(props) {
+  return (
+    <div>
+     <Child></Child>
+    </div>
+    
+    
+  );
+}
+
+export default Parent;
